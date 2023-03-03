782d3762684974069551066c667c28e0b4f381f9
LJS0000 <jisu2281l@naver.com>
2023-03-04 02:45:00 +0900

    Update: 헤더, 푸터 추가

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,3 +1,27 @@
-export default function Footer(): JSX.Element {
-  return <div>'footer'</div>;
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>홈</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>로그인</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>마이페이지</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
 }
